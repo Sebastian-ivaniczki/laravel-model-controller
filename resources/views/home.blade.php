85372076ff330518e4e85f44874ec89019da6e59
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-24 15:27:28 +0100

    add layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }}</title>
-    @vite('resources/js/app.js')
+@section('main-content')
+    <section>
 
-    @yield('cdns')
-</head>
-
-<body>
-    <h1 class="text-danger">sdfsgf</h1>
-
-    @yield('scripts')
-</body>
-
-</html>
+    </section>
+@endsection
