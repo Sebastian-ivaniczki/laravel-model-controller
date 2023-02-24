2657590293805712201710f4a306e5986102e664
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-24 16:17:34 +0100

    extension php added and take movies from db

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class myController extends Controller
@@ -9,6 +10,8 @@ class myController extends Controller
     //home
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+        //$movies = [];
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,6 @@
 @section('main-content')
     <section>
         <h1>sdkjg</h1>
+        @dd($movies)
     </section>
 @endsection
