3c5d641e7fc947d58e33d8d5f679cab0d8b7f91a
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-24 16:44:03 +0100

    add contro movies dosent exist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,21 @@
     <section>
         <h1 class="text-center my-4">Movies</h1>
         <div class="container my-4 d-flex justify-content-center flex-wrap ">
+            @isset($movies)
+                @foreach ($movies as $movie)
+                    <div class="card m-2" style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">{{ $movie->original_title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $movie->title }}</h6>
+                            <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                            <p class="card-text"> Publication date: {{ $movie->date }}</p>
+                            <p class="card-text"> Vote: {{ $movie->vote }}/10</p>
 
-            @foreach ($movies as $movie)
-                <div class="card m-2" style="width: 18rem;">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">{{ $movie->original_title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $movie->title }}</h6>
-                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
-                        <p class="card-text"> Publication date: {{ $movie->date }}</p>
-                        <p class="card-text"> Vote: {{ $movie->vote }}/10</p>
-
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @endisset
+
 
         </div>
 
